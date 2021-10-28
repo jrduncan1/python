8655ee43c0d8d2611d22691f9eb1f72acfdfd7cd
jrduncan1 <dunc5201@gmail.com>
2021-10-28 14:35:04 -0600

    understanding routings assignment

---
+++ flask/fundamentals/understanding_routings/server.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/dojo')
+def dojo():
+    return 'Dojo!'
+
+@app.route('/say/<name>')
+def say(name):
+    return "Hi, " + name
+
+@app.route('/<text>/<int:num>')
+def mult(text, num):
+    return f"{text * num}"
+
+if __name__ == "__main__":
+    app.run(debug = True)
