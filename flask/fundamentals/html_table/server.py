c3aaeb729ad118ef2b043215f808d60b80dc4b15
jrduncan1 <dunc5201@gmail.com>
2021-10-30 18:34:45 -0600

    fixed formatting on html_table

--- flask/fundamentals/html_table/server.py
+++ flask/fundamentals/html_table/server.py
@@ -4,14 +4,13 @@ app = Flask(__name__)
 @app.route('/')
 def table():
     users = [
-    {'first_name' : 'Michael', 'last_name' : 'Choi'},
-    {'first_name' : 'John', 'last_name' : 'Supsupin'},
-    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-    {'first_name' : 'KB', 'last_name' : 'Tonel'}
-    ]
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
     return render_template('index.html', users=users)
 
-
-
+# Make sure this is at the bottom of your server.py file
 if __name__=="__main__":
     app.run(debug=True)
