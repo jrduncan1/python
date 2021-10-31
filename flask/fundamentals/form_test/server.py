9f486e2aec96e071c9046f89ec3e6d47999076af
jrduncan1 <dunc5201@gmail.com>
2021-10-31 13:39:02 -0600

    added show.html & session to server

--- flask/fundamentals/form_test/server.py
+++ flask/fundamentals/form_test/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = 'dolphins'
 
 @app.route('/')
 def hello_world():
@@ -8,8 +9,14 @@ def hello_world():
 @app.route('/users', methods=['POST'])
 def create_user():
     print("Got Post Info")
-    print(request.form)
-    return redirect('/')
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    return redirect("/show")
+
+@app.route("/show")
+def show_user():
+    print("Sowing the User Info from the Form")
+    return render_template("show.html", name_on_template=session['username'], email_on_template=['useremail'])
 
 # Make sure this is at the bottom of your server.py file
 if __name__=="__main__":
