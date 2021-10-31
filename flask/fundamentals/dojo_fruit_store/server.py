ef08dab86879a666f2e7e77d90104d154a3cccfa
jrduncan1 <dunc5201@gmail.com>
2021-10-30 20:59:35 -0600

    clarified key names to be more specific

--- flask/fundamentals/dojo_fruit_store/server.py
+++ flask/fundamentals/dojo_fruit_store/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session
 from datetime import datetime
 app = Flask(__name__)
+app.secret_key = '5ar5w2f6wr8R'
 
 @app.route('/')         
 def index():
@@ -8,16 +9,16 @@ def index():
 
 @app.route('/checkout', methods=['POST'])         
 def checkout():
-    session['raspberry_count'] = request.form['raspberry']
     session['strawberry_count'] = request.form['strawberry']
+    session['raspberry_count'] = request.form['raspberry']
     session['apple_count'] = request.form['apple']
     session['blackberry_count'] = request.form['blackberry']
     session['first_name'] = request.form['first_name']
     session['last_name'] = request.form['last_name']
     session['student_id'] = request.form['student_id']
-    session['count'] = int(['strawberry_count'])+int(['raspberry_count'])+int(['apple_count'])+int(['blackberry_count'])
+    session['total_counter'] = int(session['strawberry_count'])+int(session['raspberry_count'])+int(session['apple_count'])+int(session['blackberry_count'])
     session['date'] = datetime.now()
-    print("Charging", session['first_name'], session['last_name'], "for", session['count'], "fruits on", session['date'], ".")
+    print("Charging", session['first_name'], session['last_name'], "for", session['total_counter'], "fruits on", session['date'], ".")
     return render_template("checkout.html")
 
 @app.route('/fruits')         
