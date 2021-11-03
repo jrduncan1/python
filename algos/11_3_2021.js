c0ad2b29392c4982261852e7223a853be561ccc5
jrduncan1 <dunc5201@gmail.com>
2021-11-03 11:25:08 -0600

    algo update w solution

--- algos/11_3_2021.js
+++ algos/11_3_2021.js
@@ -36,3 +36,54 @@ function parensValid(str) {
 console.log(parensValid(str1))
 
 // *****************************************************************
+
+//Instructor Solution//
+
+/* 
+Braces Valid
+Given a string sequence of parentheses, braces and brackets, determine whether it is valid. 
+*/
+
+const str1 = "W(a{t}s[o(n{ c}o)m]e )h[e{r}e]!";
+const expected1 = true;
+
+const str2 = "D(i{a}l[ t]o)n{e";
+const expected2 = false;
+
+const str3 = "A(1)s[O (n]0{t) 0}k";
+const expected3 = false;
+
+const str4 = "";
+const expected4 = true;
+
+/**
+ * Determines whether the string's braces, brackets, and parenthesis are valid
+ * based on the order and amount of opening and closing pairs.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {boolean} Whether the given strings braces are valid.
+ */
+function bracesValid(str) {
+    let stack = []
+    let opens = "([{"
+    let closeToOpen = {
+        ")" : "(",
+        "]" : "[",
+        "}" : "{",
+    }
+    for (let i = 0; i < str.length; i++){
+        if(opens.includes(str[i])){
+            stack.push(str[i])
+        } else if(str[i] in closeToOpen){
+            if(closeToOpen[str[i]] === stack[stack.length - 1]){
+                stack.pop()
+            } else {
+                return false
+            }
+        }
+    }
+    return stack.length === 0
+}
+
+console.log(bracesValid(str4))
